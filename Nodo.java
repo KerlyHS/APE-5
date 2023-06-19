ca64f6631a304151c5e68f99be1a18f970c84795
Estefania2002 <84207188+E2002-debug@users.noreply.github.com>
2023-06-19 16:08:18 -0500

    Lista Doblemente Enlazada

---
+++ Nodo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listadoblementeenlazada;
+
+/**
+ *
+ * @author Usuario_PC
+ */
+public class Nodo {
+     int dato;
+    Nodo anterior;
+    Nodo siguiente;
+    Nodo ultimo;
+
+}
