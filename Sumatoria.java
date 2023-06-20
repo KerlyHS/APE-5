a6a17e27b94ea5e704b3c73f76e8eeb649b7e017
Mariuxi Alexandra Valdez Cuenca <134022113+mariuxi04@users.noreply.github.com>
2023-06-19 19:45:40 -0500

    Promedio

---
+++ Sumatoria.java
@@ -0,0 +1,11 @@
+class Sumatoria {
+    private double sumatoria;
+
+    public void agregarTermino(double valor) {
+        sumatoria += valor;
+    }
+
+    public double obtenerSumatoria() {
+        return sumatoria;
+    }
+}
