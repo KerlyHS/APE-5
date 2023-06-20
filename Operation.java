d69d3659c53ac8edec373b165dd60318aa42b253
Mariuxi Alexandra Valdez Cuenca <134022113+mariuxi04@users.noreply.github.com>
2023-06-19 19:41:22 -0500

    Suma

---
+++ Operation.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author Admin
+ */
+public class Operation {
+    
+    public int sumar (int n1, int n2){
+        int respuesta = 0;
+        respuesta = n1 + n2;
+        return respuesta;
+    }
+
+}
+    
